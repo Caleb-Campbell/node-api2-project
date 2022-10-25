9bb5accc679ef81c8c47d75a4e614b99086ccb39
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-10-25 14:58:39 -0600

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+
+
+const express = require('express')
+const postRouter = require('./posts/posts-router')
+
+const server = express()
+server.use(express.json())
+server.use('/api/posts', postRouter)
+
+
+server.get('/', (req, res) => {
+    res.json({
+        message: 'welcome to the express server'
+    })
+})
+
+module.exports = server
